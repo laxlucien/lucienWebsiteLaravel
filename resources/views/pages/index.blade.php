3b7428d6f9cf5b00524823ce913b4c1d76e31b2a
laxlucien <laxlucien@gmail.com>
2023-12-04 01:25:08 -0800

    added some things

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+@extends('pages.index')
+
+@section('navBar')
+
+<script src="JavaScript/mainBackground.js"></script>
+<div class="container">
+	<span class="title">Lucien's Place For Things</span>
+</div>
+
+@endsection
