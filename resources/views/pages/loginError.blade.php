bc2db7b08d32afa16ba3a2bf5371cbf9ba2b224d
laxlucien <laxlucien@gmail.com>
2023-12-08 00:20:30 -0800

    Fixed a couple things that were bugging me

--- resources/views/pages/loginError.blade.php
+++ resources/views/pages/loginError.blade.php
@@ -4,9 +4,9 @@
 
 <br>
     <center>
-    <h3 style="color:white">ERROR:<br><br>Incorrect Username or Password<br>Or<br>You are trying to log into the system too soon after account creation.</h3>
+    <h3 style="width: 70%; color:white; background-color: #27272A; padding: 10px; border-radius: 8px;">ERROR:<br><br>Incorrect Username or Password<br>Or<br>You are trying to log into the system too soon after account creation.</h3>
     <br>
-    <h3 style="color:white">Click <u><a href="{{ url('/login') }}">here</a></u> to login again<br>or<br>Click <u><a href="{{ url('/') }}">here</a></u> to return to home.</h3>
+    <h3 style="width: 70%; color:white; background-color: #27272A; padding: 10px; border-radius: 8px;">Click <u><a href="{{ url('/login') }}">here</a></u> to login again<br>or<br>Click <u><a href="{{ url('/') }}">here</a></u> to return to home.</h3>
     </center>
 
 @endsection
--- resources/views/pages/photoWall.blade.php
+++ resources/views/pages/photoWall.blade.php
@@ -3,11 +3,16 @@
 @section('content')
 
 <center>
-    <h1 style="width: 20%; color:white; background-color: #27272A; padding: 10px; border-radius: 8px;">Photos -</h1>
-    <br>
-    <h3 style="width: 60%; color:white; background-color: #27272A; padding: 10px; border-radius: 8px;">
-    Browse the various photos that various people have uploaded to the website below
-    </h3>
+    <h1 style="width: 15%; color:white; background-color: #27272A; padding: 10px; border-radius: 8px;">Photos -</h1>
+    @guest
+        <h3 style="width: 60%; color:white; background-color: #27272A; padding: 10px; border-radius: 8px;">
+            Browse the various photos that various people have uploaded to the website below
+        </h3>
+    @else
+        <h3 style="width: 60%; color:white; background-color: #27272A; padding: 10px; border-radius: 8px;">
+            Hello {{Auth::user()->username}}, would you like to upload a photo?
+        </h3>
+    @endguest
     <br>
 </center>
 
